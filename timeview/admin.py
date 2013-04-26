217239e1d9891940348420d582d374aa8b5439aa
Ian Hung <ianmenghung@gmail.com>
2013-04-25 22:38:36 -0400

    Basic model for time and added validation for time model.

---
+++ timeview/admin.py
@@ -0,0 +1,10 @@
+'''
+Created on 2013-04-25
+
+@author: Ian
+'''
+from django.contrib import admin
+from timeview.models import Timelike
+
+admin.site.register(Timelike)
+
--- timeview/models.py
+++ timeview/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 class Timelike(models.Model):
     title = models.CharField(max_length=200)
-    localsource = models.FilePathField(null=True)
-    youtubesource = models.URLField(max_length=200)
+    localsource = models.FileField(upload_to='video/timelike', blank=True)
+    youtubesource = models.URLField(max_length=200, blank=True)
+    vimeosource = models.URLField(max_length=200, blank=True)
     
+    def __str__(self):
+        return self.title
     
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        #allow only one source for video/audio
+        
+        if (int(not self.localsource) + int(not self.youtubesource) + int(not self.vimeosource)) != 2:
+            raise ValidationError('Please select exactly one source for the video')
+        
