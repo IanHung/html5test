a971d96de3263a29baf02f9d0132325d53863206
Ian Hung <ianmenghung@gmail.com>
2013-05-09 00:15:16 -0400

    Setup a dropdown menu for both full and live transcripts.  Added Full functionality to the full transcript.

--- static/html5test/js/custom.js
+++ static/html5test/js/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	$('.col2').height($('.col1').height()-20);
+	$('.col3').height(($('.col1').height()-150));
 	$(window).resize(function(){
 		$('.col2').height($('.col1').height()-20);
+		$('.col3').height(($('.col1').height()-150));
 	});
 });
