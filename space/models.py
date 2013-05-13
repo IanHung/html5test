aaf9fac474591a0f740bb559019fac9b59adb4a5
Tingish <tingish@gmail.com>
2013-05-13 01:33:15 -0400

    May 12th, new models for image and paragraph have been added. Changed a bit of the text commenting menu.

--- space/models.py
+++ space/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from forum.models import Comment
+from django.contrib.contenttypes import generic
 
 # Create your models here.
+class Paragraph(models.Model):
+    #basic fields
+    author = models.ForeignKey(User, blank=True, null=True)
+    comment = generic.GenericRelation(Comment)
+
+class Imageclass(models.Model):
+    #basic fields
+    author = models.ForeignKey(User, blank=True, null=True)
+    comment = generic.GenericRelation(Comment)
+    localsource = models.FileField(upload_to='image/spacelike', blank=True)
+    linksource = models.URLField(max_length=200, blank=True) 
+    
+def __str__(self):
+    return self.title
